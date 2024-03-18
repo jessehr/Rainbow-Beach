b5973b3e2c18e2ea0d3753fd185c97eebe1cca3b
jessehr <jessehrapoport@gmail.com>
2024-03-18 00:42:06 -0700

    Initial Commit

---
+++ PuzzleGame/PuzzleGameApp.swift
@@ -0,0 +1,32 @@
+//
+//  PuzzleGameApp.swift
+//  PuzzleGame
+//
+//  Created by Jesse R on 3/18/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct PuzzleGameApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
