9169dcbcb90ce2f327b9b39665d15fc6071b8d9a
jessehr <jessehrapoport@gmail.com>
2024-03-31 13:42:36 -0700

    PUZ-29 separate time into its own manager and fix sound not restarting bug

--- PuzzleGame/Managers/SoundManager.swift
+++ PuzzleGame/Managers/SoundManager.swift
@@ -12,55 +12,41 @@ import UIKit
 class SoundManager {
     private let filename: String
     private let fileType: String
-    private var player: AVAudioPlayer?
-    private var isPlaying = false
+    private var player: AVAudioPlayer!
     private var stopTimer: Timer?
+    private var timeManager: TimeManager
     
-    private static func getPreparedPlayer(filename: String, fileType: String) -> AVAudioPlayer? {
+    init(filename: String, fileType: String = "mp3") {
+        self.filename = filename
+        self.fileType = fileType
+        self.timeManager = TimeManager()
+        self.player = nil
+        
+        // now that everything is initialized, use instance vars
+        self.timeManager.callback = self.pause
+        self.player = makePlayer(filename: filename, fileType: fileType)
+    }
+    
+    func play(for durationInSeconds: TimeInterval) {
+        if !player.isPlaying {
+            player.play()
+        }
+        timeManager.setOrResetTimer(for: durationInSeconds)
+    }
+            
+    private func play() {
+        player.play()
+    }
+    
+    private func pause() {
+        player.pause()
+        player.currentTime = 0
+    }
+    
+    private func makePlayer(filename: String, fileType: String) -> AVAudioPlayer? {
         guard let url = Bundle.main.url(forResource: filename, withExtension: fileType) else { return nil }
         let player = try? AVAudioPlayer(contentsOf: url)
         player?.prepareToPlay()
         return player
-    }
-    
-    init(filename: String, fileType: String = "mp3") {
-        self.player = Self.getPreparedPlayer(filename: filename, fileType: fileType)
-        self.filename = filename
-        self.fileType = fileType
-    }
-    
-    func play(for durationInSeconds: TimeInterval, reset: Bool = true) {
-        if isPlaying {
-            extendPlayTime(for: durationInSeconds)
-        } else {
-            playFromStart(for: durationInSeconds)
-        }
-    }
-    
-    private func extendPlayTime(for durationInSeconds: TimeInterval) {
-        self.stopTimer?.invalidate()
-        self.stopTimer = makeStopTimer(for: durationInSeconds)
-    }
-    
-    private func playFromStart(for durationInSeconds: TimeInterval) {
-        play()
-        self.stopTimer = makeStopTimer(for: durationInSeconds)
-    }
-    
-    private func play() {
-        isPlaying = true
-        player?.play()
-    }
-    
-    private func pause() {
-        player?.pause()
-        player?.currentTime = 0
-        isPlaying = false
-    }
-    
-    private func makeStopTimer(for durationInSeconds: TimeInterval) -> Timer {
-        return Timer.scheduledTimer(withTimeInterval: durationInSeconds, repeats: false) { _ in
-            self.pause()
-        }
     }
 }
