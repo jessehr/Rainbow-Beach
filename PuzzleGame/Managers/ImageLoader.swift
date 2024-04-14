7e68e40d41d4940a1fa838eec653f13808d09cfa
jessehr <jessehrapoport@gmail.com>
2024-04-14 12:26:24 -0700

    PUZ-57 add image loader

---
+++ PuzzleGame/Managers/ImageLoader.swift
@@ -0,0 +1,29 @@
+//
+//  ImageLoader.swift
+//  PuzzleGame
+//
+//  Created by Jesse R on 4/14/24.
+//
+
+import Foundation
+import SwiftUI
+
+class ImageLoader {
+    func image(for stringToHash: String) -> Image? {
+        guard let sandImageCount else { return nil }
+        let hashedString = stringToHash.hashValue
+        let sandImageNumber = abs(hashedString) % sandImageCount
+        return Image("sand\(sandImageNumber)")
+    }
+    
+    var sandImageCount: Int? {
+        var count = 0
+        for i in 1...10000 {
+            guard UIImage(named: "sand\(i)") != nil else {
+                return count
+            }
+            count += 1
+        }
+        return nil
+    }
+}
