2ff911b63ed029f0a32c849033039f69e54ce608
jessehr <jessehrapoport@gmail.com>
2024-04-14 13:51:46 -0700

    PUZ-34 clean up a lil

--- PuzzleGame/Managers/MapLoader.swift
+++ PuzzleGame/Managers/MapLoader.swift
@@ -9,17 +9,22 @@ import Foundation
 
 class MapLoader {
     static func loadMap(levelNumber: Int) throws -> Map {
-        guard let path = Bundle.main.path(forResource: "level\(levelNumber)", ofType: "txt") else {
-            throw GeneralError.generalError
-        }
-        let fileContents = try String(contentsOfFile: path, encoding: String.Encoding.utf8)
-        let rowStrings = fileContents.components(separatedBy: .newlines)
+        let rowStrings = try getRowStrings(for: levelNumber)
         let dividerIndex = Int(rowStrings.firstIndex(of: "Map:")!)
         let gamePieceStrings = Array(rowStrings[1..<dividerIndex])
         let mapStrings = Array(rowStrings[(dividerIndex + 1)...])
         let mapDepths = rowStringsToInts(rowStrings: mapStrings)
         let gamePiece = GamePiece(from: gamePieceStrings)
         return Map(depths: mapDepths, gamePiece: gamePiece)
+    }
+    
+    private static func getRowStrings(for levelNumber: Int) throws -> [String] {
+        guard let path = Bundle.main.path(forResource: "level\(levelNumber)", ofType: "txt") else {
+            throw GeneralError.generalError
+        }
+        let fileContents = try String(contentsOfFile: path, encoding: String.Encoding.utf8)
+        let rowStrings = fileContents.components(separatedBy: .newlines)
+        return rowStrings
     }
     
     private static func rowStringsToInts(rowStrings: [String]) -> [[Int]] {
