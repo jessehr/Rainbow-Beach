7f836437c00e83d77349b2d622519358ba97109d
jessehr <jessehrapoport@gmail.com>
2024-04-21 20:05:45 -0700

    save starting location of each point

--- PuzzleGame/Models/TouchPoint.swift
+++ PuzzleGame/Models/TouchPoint.swift
@@ -8,7 +8,16 @@
 import Foundation
 
 struct TouchPoint: Equatable, Identifiable {
-    let creationTime = Date.now
-    var id = UUID()
+    let creationTime: Date
+    let id: UUID
+    let startingPoint: CGPoint
+    
     var point: CGPoint
+    
+    init(point: CGPoint) {
+        self.id = UUID()
+        self.creationTime = Date.now
+        self.startingPoint = point
+        self.point = point
+    }
 }
